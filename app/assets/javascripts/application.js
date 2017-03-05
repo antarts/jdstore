52c0e21f702e2cc1f4bff6aea1e97c0f79278deb
Antarts <m.dream@foxmail.com>
2017-03-05 17:04:28 +0800

    design singin&signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,32 +19,13 @@
 //= require_tree .
 
 $(function() {
-
-    $('a[href="#toggle-search"], .navbar-bootsnipp .bootsnipp-search .input-group-btn > .btn[type="reset"]').on('click', function(event) {
-		event.preventDefault();
-		$('.navbar-bootsnipp .bootsnipp-search .input-group > input').val('');
-		$('.navbar-bootsnipp .bootsnipp-search').toggleClass('open');
-		$('a[href="#toggle-search"]').closest('li').toggleClass('active');
-
-		if ($('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
-			/* I think .focus dosen't like css animations, set timeout to make sure input gets focus */
-			setTimeout(function() {
-				$('.navbar-bootsnipp .bootsnipp-search .form-control').focus();
-			}, 100);
-		}
-	});
-
-	$(document).on('keyup', function(event) {
-		if (event.which == 27 && $('.navbar-bootsnipp .bootsnipp-search').hasClass('open')) {
-			$('a[href="#toggle-search"]').trigger('click');
-		}
-	});
-
+  $('.js-nav a, .js-connect').click(function(e) {
+    e.preventDefault();
+    $('body, html').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 750);
+  });
 });
-
-$(document).on('click', '#gotop', function () {
-  $('body').animate({'scrollTop': 0}, 500) //在500ms的时间内，慢慢地回到顶部
-})
 
 // /*Tooltip*/
 // $(function () {
