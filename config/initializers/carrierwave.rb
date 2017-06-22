05bb81bcc8fc4b05d08fcc86cb29eedd95177d8a
Antarts <m.dream@foxmail.com>
2017-06-22 09:56:50 +0800

    fix aws wrong

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,7 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_provider = 'fog'
@@ -7,7 +11,7 @@ CarrierWave.configure do |config|
 
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
 
-      region:                ENV["AWS_REGION"]
+      region:                'ap-northeast-1'
 
     }
     config.fog_directory  = ENV["AWS_BUCKET_NAME"]
